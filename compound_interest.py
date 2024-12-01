5747f800ccb191f607844e0869cba0f91640cac9
yohelperez <perezyohel@gmail.com>
2024-12-01 14:03:03 -0500

    feat: add compound_interest script

---
+++ compound_interest.py
@@ -0,0 +1,12 @@
+# Datos iniciales
+monto_inicial = 1000000  # COP
+tasa_anual = 0.13  # 13% EA
+dias = 14
+
+# Cálculo de tasa diaria
+tasa_diaria = tasa_anual / 365
+
+# Interés compuesto para n días
+intereses = monto_inicial * ((1 + tasa_diaria) ** dias - 1)
+interes_anual = monto_inicial * tasa_anual
+print(f"intereses a los {dias} dias: {intereses} \ninteres anual: {interes_anual}")
